64c285bd59f7b5293735da31ee5770f51e40eabf
Raul Sofia <rauljcsofia@gmail.com>
2022-11-11 21:27:28 +0000

    dfbfb

--- setup.py
+++ setup.py
@@ -12,12 +12,12 @@ setup(
     description='Pacote que permite gerar paginas html a partir de dataframes de moleculas, apresentar as suas propriedades, selecionar e descarregar em varios formatos, para acompanhar e apresentar resultados de variados modelos moleculares, sempre em grande estilo, com o visual ChEMBL.',
     long_description=open('README.txt').read(),
     install_requires=[
-        "beautifulsoup4==4.11.1"
-        "numpy==1.20.3"
-        "pandas==1.3.4"
-        "rdkit==2022.9.1"
-        "setuptools==58.0.4"
-        "tqdm==4.62.3"
+        "beautifulsoup4"
+        "numpy"
+        "pandas"
+        "rdkit"
+        "setuptools"
+        "tqdm"
     ]
     
 )
