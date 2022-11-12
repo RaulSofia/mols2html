860db94a1868a68ef7e7abc34b6702eae4b53c2c
Raul Sofia <rauljcsofia@gmail.com>
2022-11-12 03:21:29 +0000

    9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='mols2html',
-    version='1.0.8',
+    version='1.0.9',
     author='Raul Sofia',
     author_email='rauljcsofia.com',
     packages=["mols2html"],
