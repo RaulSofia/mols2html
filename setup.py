6d4df7f41a3b0cee2eea39d610282a7f865e393b
Raul Sofia <67133355+RaulSofia@users.noreply.github.com>
2022-11-12 02:52:47 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='mols2html',
-    version='1.0.1',
+    version='1.0.2',
     author='Raul Sofia',
     author_email='rauljcsofia.com',
     packages=find_packages(where="mols2html"),
