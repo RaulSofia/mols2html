9b97cb1d3b3db95e3cd4adbe1cc8bbe2170e8d15
Raul Sofia <rauljcsofia@gmail.com>
2022-11-12 03:28:10 +0000

    b cb bc

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import find_packages, setup
 
 setup(
     name='mols2html',
-    version='1.0.11',
+    version='1.0.13',
     author='Raul Sofia',
     author_email='rauljcsofia.com',
     packages=["mols2html"],
     scripts=[],
-    package_dir={"": "index.html"},
+    package_dir={"": "."},
     include_package_data=True,
+    package_data={"mols2html": ["*.html"]},
     url='https://github.com/RaulSofia/mols2html',
     license='LICENSE.txt',
     description='Pacote que permite gerar paginas html a partir de dataframes de moleculas, apresentar as suas propriedades, selecionar e descarregar em varios formatos, para acompanhar e apresentar resultados de variados modelos moleculares, sempre em grande estilo, com o visual ChEMBL.',
